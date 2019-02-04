9432900dd752b3ea79ccc5e077b8698f7cdfb43f
Jonathan Hong <jonhong777@gmail.com>
2019-02-04 04:18:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function addNewElementAsLi(){
 
 function addNewLiOnClick(){
   let submit = document.querySelector("input[type = 'submit']");
-  document.addEventListener('click', function(){
-    
+  submit.addEventListener('click', function(){
+    addNewElementAsLi();
   })
 }
